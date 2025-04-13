7774b3a83bb9b97c346119b1fdab6c1059dd9933
Kirito0098 <lexa.sirov@gmail.com>
2025-04-13 19:06:18 +0300

    23123

--- adminpanel.sh
+++ adminpanel.sh
@@ -172,7 +172,7 @@ EOL
     systemctl restart nginx
     
     # Получение сертификата
-    certbot certbot --nginx -d $DOMAIN --non-interactive --agree-tos -m $EMAIL --http-01-port=8443 >/dev/null 2>&1
+    certbot certbot --nginx -d $DOMAIN --non-interactive --agree-tos -m $EMAIL --http-01-port=8443
     check_error "Не удалось получить сертификат Let's Encrypt"
     
     # Удаляем старый файл с SSL-параметрами, чтобы избежать дублирования

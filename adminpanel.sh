18e8787cbd9b303b3869aef0659c1b2a30519df9
Kirito0098 <58187639+Kirito0098@users.noreply.github.com>
2025-04-14 00:11:33 +0300

    Update adminpanel.sh

--- adminpanel.sh
+++ adminpanel.sh
@@ -172,7 +172,7 @@ EOL
     systemctl restart nginx
     
     # Получение сертификата
-    certbot certbot --nginx -d $DOMAIN --non-interactive --agree-tos -m $EMAIL --http-01-port=8443
+    certbot --nginx -d $DOMAIN --non-interactive --agree-tos -m $EMAIL
     check_error "Не удалось получить сертификат Let's Encrypt"
     
     # Удаляем старый файл с SSL-параметрами, чтобы избежать дублирования

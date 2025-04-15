f11df8a7ca5e82d2fac21b4bee8541efa4a56d9a
Kirito0098 <lexa.sirov@gmail.com>
2025-04-15 21:55:20 +0300

    423423423

--- lib/install.sh
+++ lib/install.sh
@@ -190,14 +190,14 @@ install() {
 
     # Проверка наличия файла .env
     if [ -f "$INSTALL_DIR/.env" ]; then
-        echo "${YELLOW}.env файл существует, добавляем значения, если их нет...${NC}"
+        echo "${YELLOW} Добавляем значения в файл .env...${NC}"
         
         # Проверяем, есть ли уже строки SECRET_KEY и APP_PORT
         grep -qxF "SECRET_KEY='$SECRET_KEY'" "$INSTALL_DIR/.env" || echo "SECRET_KEY='$SECRET_KEY'" >> "$INSTALL_DIR/.env"
         grep -qxF "APP_PORT=$APP_PORT" "$INSTALL_DIR/.env" || echo "APP_PORT=$APP_PORT" >> "$INSTALL_DIR/.env"
     else
         # Если файл не существует, создаем его с необходимыми значениями
-        echo "${YELLOW}.env файл не найден, создаем новый...${NC}"
+        echo "${YELLOW} Файл .env не найден, создаем новый...${NC}"
         cat > "$INSTALL_DIR/.env" <<EOL
 SECRET_KEY='$SECRET_KEY'
 APP_PORT=$APP_PORT
